24579754b18e464f961df31e8f5c782e9207d64a
sevena-s <sevenasandhu@gmail.com>
2022-01-08 20:15:17 -0800

    added functionality and styling

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 function makeGrid(){
     const grid = document.querySelector('#container');
 
-    const item = document.createElement('div');
-    item.classList.add('item');
-    item.style.cssText = `grid-column-start: ${1}; grid-column-end: ${2};`;
-    item.textContent = '1';
+    for(let i = 1; i < 53; i++){
+        for(let j = 1; j < 53; j++){
+            const item = document.createElement('div');
+            item.classList.add('item');
+            item.style.cssText = `grid-row-start: ${i}; grid-column-end: ${j};`;
+            item.setAttribute('id', 'item');  
+            grid.appendChild(item);
+        }
+    }
 
-    grid.appendChild(item);
 }
 
 makeGrid();
+
+const divs = document.querySelectorAll('#item');
+
+divs.forEach(box => {
+    box.addEventListener('mouseenter', () => {
+        console.log(box.id);
+        box.classList.add('pressed');
+    });
+})
