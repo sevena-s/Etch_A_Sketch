984e2122aefb76d1136fabf6bc6de2c14711441c
sevena-s <sevenasandhu@gmail.com>
2022-01-08 23:03:09 -0800

    fixed box sizes

--- script.js
+++ script.js
@@ -12,21 +12,23 @@ function makeGrid(boxes){
         for(let j = 1; j < (boxes + 1); j++){
             const item = document.createElement('div');
             item.classList.add('item');
-            item.style.cssText = `grid-row-start: ${i}; grid-column-end: ${j};`;
+            item.style.cssText = `grid-row-start: ${i}; grid-column-end: ${j}; grid-template-columns: repeat(${i}, 80px); grid-template-rows: repeat(${i}, 80px);`;
             item.setAttribute('id', 'item');  
             grid.appendChild(item);
         }
     }
 };
 
+function addBoxes(boxes, items){
+    for(let i = 1; i < (boxes + 1); i++){
+        for(let j = 1; j < (boxes + 1); j++){
+            grid.appendChild(items[i]);
+        }
+    }
+}
+
 makeGrid(squares);
 let divs = document.querySelectorAll('#item');
-slider.oninput = function() {
-    sliderVal.textContent = slider.value;
-    squares = slider.value;
-    //makeGrid(squares);
-};
-
 
 function clearBoxes(){
     divs.forEach(function(item){
@@ -35,6 +37,17 @@ function clearBoxes(){
 };
 
 clear.addEventListener('click', clearBoxes);
+
+slider.oninput = function() {
+    sliderVal.textContent = slider.value;
+    squares = slider.value;
+    divs.forEach(function(item){
+        item.classList.remove('pressed');
+        grid.removeChild(item);
+    });
+    console.log(squares);
+    addBoxes(squares, divs);
+};
 
 divs.forEach(box => {
     box.addEventListener('mouseenter', () => {
