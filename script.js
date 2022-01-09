09dc469d19506656ebf865bacbd03da1145a93d6
sevena-s <sevenasandhu@gmail.com>
2022-01-08 22:22:38 -0800

    added clear button function

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-function makeGrid(){
-    const grid = document.querySelector('#container');
+const slider = document.querySelector('#slider');
+const sliderVal = document.querySelector('.val');
+const grid = document.querySelector('#container');
+const clear = document.querySelector('.clear');
 
-    for(let i = 1; i < 53; i++){
-        for(let j = 1; j < 53; j++){
+sliderVal.textContent = slider.value;
+let squares = 40;
+
+function makeGrid(boxes){
+
+    for(let i = 1; i < (boxes + 1); i++){
+        for(let j = 1; j < (boxes + 1); j++){
             const item = document.createElement('div');
             item.classList.add('item');
             item.style.cssText = `grid-row-start: ${i}; grid-column-end: ${j};`;
@@ -10,12 +17,24 @@ function makeGrid(){
             grid.appendChild(item);
         }
     }
+};
 
-}
+makeGrid(squares);
+let divs = document.querySelectorAll('#item');
+slider.oninput = function() {
+    sliderVal.textContent = slider.value;
+    squares = slider.value;
+    //makeGrid(squares);
+};
 
-makeGrid();
 
-const divs = document.querySelectorAll('#item');
+function clearBoxes(){
+    divs.forEach(function(item){
+        item.classList.remove('pressed');
+    });
+};
+
+clear.addEventListener('click', clearBoxes);
 
 divs.forEach(box => {
     box.addEventListener('mouseenter', () => {
